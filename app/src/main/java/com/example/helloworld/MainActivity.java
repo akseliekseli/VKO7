619579eaa27af701d5d9c9c7f613e1bfffb3b583
Akseli Suutari <61748148+audioAXS@users.noreply.github.com>
2020-07-26 20:55:33 +0300

    Valmista

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    TextView text2;
+    EditText update_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textView);
+        text2 = (TextView) findViewById(R.id.textView2);
+        update_text = (EditText) findViewById(R.id.textInput);
+        update_text.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                text.setText(update_text.getText());
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    public void printHW(View v){
+        System.out.println("Hello World!");
+        text2.setText("Hello World!");
+        text.setText(update_text.getText());
     }
 }
